f0408c5a1aa3445cfa2509e83e84686a6e0ab20a
lcoin-after-party <245492721+lcoin-after-party@users.noreply.github.com>
2025-12-06 18:53:34 +0100

    fix : urgent Fix to limit the message's great powers

--- features/sendMGS.js
+++ features/sendMGS.js
@@ -1,5 +1,5 @@
 async function sendMSG(message,client,PREFIX){
-    if(message.author.id == 399199197938712587 || message.author.id == 733414175556239525){
+    if(message.guild.id == 1422772405138493461 && (message.author.id == 399199197938712587 || message.author.id == 733414175556239525)){
         try {
             
             const [_ ,channedlink , ...restMessage ] = message.content?.slice(PREFIX.length).split(" ")
