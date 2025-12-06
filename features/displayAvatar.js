de19690ce4bc48a0cdeae10a32419bd035093041
lcoin-after-party <245492721+lcoin-after-party@users.noreply.github.com>
2025-12-06 18:16:01 +0000

    fix: misspelling

--- features/displayAvatar.js
+++ features/displayAvatar.js
@@ -2,7 +2,7 @@ async function displayAvatar(message) {
     const target = message.mentions.members.first()
     if (target) {
         const avatar = target.displayAvatarURL({ size: 1024, dynamic: true });
-        message.rely(avatar);
+        message.reply(avatar);
     } else {
         const avatar = message.author.displayAvatarURL({ size: 1024, dynamic: true });
         message.reply(avatar);
